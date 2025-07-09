9ddba7bb82c79332fa5debaaddef81cb53704250
Andrea Shtjefni <andreashtjefni14@gmail.com>
2025-07-09 11:27:23 +0200

    Add files via upload

---
+++ Adding a column and adding values.sql
@@ -0,0 +1,48 @@
+ALTER TABLE Kursi
+ADD programminglanguage VARCHAR(100);
+
+UPDATE Kursi
+SET programminglanguage = 'Java'
+WHERE id = 245;
+
+UPDATE Kursi
+SET programminglanguage = 'Java'
+WHERE id = 687;
+
+UPDATE Kursi
+SET programminglanguage = 'SQL'
+WHERE id = 732;
+
+UPDATE Kursi
+SET programminglanguage = 'Java'
+WHERE id = 910;
+
+UPDATE Kursi
+SET programminglanguage = 'Java'
+WHERE id = 101;
+
+UPDATE Kursi
+SET programminglanguage = 'Python'
+WHERE id = 124;
+
+UPDATE Kursi
+SET programminglanguage = 'CSS'
+WHERE id = 364;
+
+UPDATE Kursi
+SET programminglanguage = 'Swift'
+WHERE id = 434;
+
+UPDATE Kursi
+SET programminglanguage = 'Python'
+WHERE id = 511;
+
+UPDATE Kursi
+SET programminglanguage = 'Python'
+WHERE id = 898;
+
+
+SELECT *  FROM Kursi;
+
+ 
+ 
---
+++ Altering one of the colums.sql
@@ -0,0 +1,22 @@
+UPDATE Kursi
+SET NameOfThecourse = 'Introduction to Programming'
+WHERE ID = 124;
+UPDATE Kursi
+SET NameOfThecourse = 'Web Development'
+WHERE ID = 364;
+UPDATE Kursi
+SET NameOfThecourse = 'Mobile App Development'
+WHERE ID = 434;
+UPDATE Kursi
+SET NameOfThecourse = 'AI Fundamentals'
+WHERE ID = 511;
+UPDATE Kursi
+SET NameOfThecourse = 'Statistical Computing'
+WHERE ID = 898;
+
+
+SELECT * FROM Kursi;
+
+
+ 
+ 
